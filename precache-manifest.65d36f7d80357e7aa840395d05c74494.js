1ff282caa19c741f79b2d64f38ae2fbbd1359825
daniela rodriguez <daniela_rodriguez1990@outlook.com>
2019-08-17 22:49:24 -0400

    Updates

---
+++ precache-manifest.65d36f7d80357e7aa840395d05c74494.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f29841ccd414abb9baf16ed5a7cfd975",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "40239ec66bcd857444fc",
+    "url": "/clicky-game/static/css/main.69cb2777.chunk.css"
+  },
+  {
+    "revision": "e6b3dcb906afea788af0",
+    "url": "/clicky-game/static/js/2.8ba1fec2.chunk.js"
+  },
+  {
+    "revision": "40239ec66bcd857444fc",
+    "url": "/clicky-game/static/js/main.c246d2d6.chunk.js"
+  },
+  {
+    "revision": "e900226b6c6e02320770",
+    "url": "/clicky-game/static/js/runtime~main.98b6a1fe.js"
+  },
+  {
+    "revision": "62983245946b45ce1eedf02e393226f3",
+    "url": "/clicky-game/static/media/gorilla.62983245.jpeg"
+  },
+  {
+    "revision": "2eddac1221f81b2d462a9c5c39d8f501",
+    "url": "/clicky-game/static/media/koala.2eddac12.jpeg"
+  },
+  {
+    "revision": "86d6e138819bc8d29b478c722ecf54de",
+    "url": "/clicky-game/static/media/tiger.86d6e138.jpeg"
+  }
+]);
